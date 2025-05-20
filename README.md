88476ab423679460e0a1912f558308657295b0a0
Raz Nissim <raznissim@gmail.com>
2025-05-20 17:11:56 +0300

    Initial commit, working app, bare bones.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+---
+title: OSINT_Investigator
+app_file: app.py
+sdk: gradio
+sdk_version: 5.30.0
+---
 # OSINT_Agent
---
+++ investigators/src/investigators/main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import os
+import sys
+import warnings
+
+from datetime import datetime
+
+from investigators.crew import Investigators
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# Create output directory if it doesn't exist
+os.makedirs('output', exist_ok=True)
+
+def run():
+    """
+    Run the crew.
+    """
+    # inputs = {
+    #     'target': 'Raz Nissim',
+    #     'affiliations': 'Ben Gurion University, General Motors',
+    #     # 'current_year': str(datetime.now().year)
+    # }
+
+    # inputs = {
+    #     'target': 'Avraham Hirschson',
+    #     'affiliations': 'Israeli government, Israeli Knesset, Histadrut',
+    #     # 'current_year': str(datetime.now().year)
+    # }
+
+    inputs = {
+        'target': 'Igal Nissim',
+        'affiliations': 'Comverse, Verint',
+        # 'current_year': str(datetime.now().year)
+    }
+
+    # inputs = {
+    #     'target': 'Yeela Harel',
+    #     'affiliations': 'Israel Ministry of Justice, ThetaRay, Bank Of Israel',
+    #     # 'current_year': str(datetime.now().year)
+    # }
+
+    # inputs = {
+    #     'target': 'Yehuda Harel',
+    #     'affiliations': 'Hapoalim Bank',
+    #     # 'current_year': str(datetime.now().year)
+    # }
+
+    try:
+        Investigators().crew().kickoff(inputs=inputs)
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
+
+
