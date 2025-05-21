3a213a36014d1178770fc833ddb99adb2365cffe
Raz Nissim <raznissim@gmail.com>
2025-05-21 10:41:32 +0300

    Added logo and changed layout to gr.blocks, making it more flexible.

--- app.py
+++ app.py
@@ -15,16 +15,59 @@ def investigate(target_name, affiliations):
     
     return crew_output.raw
 
-view = gr.Interface(
-    fn=investigate,
-    inputs=[
-        gr.Textbox(label="Target name:"),
-        gr.Textbox(label="Target Affiliations (comma separated):")],
-    outputs=[gr.Markdown(label="Risk Assessment Report:")],
-    flagging_mode="never",
-    examples=[
-        ["Raz Nissim", "Ben Gurion University, General Motors"],
-    ],
-    title="OSINT Investigator",
-    description="Enter the name of your target and their affiliations (to make search easier), and get a AML Risk assessment based on their public information.",)
+# view = gr.Interface(
+#     fn=investigate,
+#     inputs=[
+#         gr.Textbox(label="Target name:"),
+#         gr.Textbox(label="Target Affiliations (comma separated):")],
+#     outputs=[gr.Markdown(label="Risk Assessment Report:")],
+#     flagging_mode="never",
+#     examples=[
+#         ["Raz Nissim", "Ben Gurion University, General Motors"],
+#     ],
+#     title="OSINT Investigator",
+#     description="Enter the name of your target and their affiliations (to make search easier), and get a AML Risk assessment based on their public information.",
+#     layout="vertical")
+# 
+# view.launch(inbrowser=True)
+
+    # Clear button functionality
+def clear_inputs():
+    return "", ""
+    
+with gr.Blocks() as view:
+    gr.Markdown("# OSINT Investigator")
+    gr.Markdown("#### Enter the name of your target and their affiliations (to make search easier), and get a AML Risk assessment based on their public information.")
+    with gr.Row(equal_height=True):
+        with gr.Column(scale=3):
+            name_input = gr.Textbox(label="Target name:")
+            affiliation_input = gr.Textbox(label="Target Affiliations (comma separated):")
+            with gr.Row():
+                clear_btn = gr.Button("Clear")
+                submit_btn = gr.Button("Investigate")
+        with gr.Column(scale=1):
+            img1 = gr.Image("images/logo1.png", show_download_button=False, show_fullscreen_button=False, show_label=False, show_share_button=False)
+        
+    
+    with gr.Row():
+        output = gr.Markdown(label="Risk Assessment Report:")
+    
+    submit_btn.click(
+        fn=investigate, 
+        inputs=[name_input, affiliation_input],
+        outputs=output
+    )
+    
+
+    
+    clear_btn.click(
+        fn=clear_inputs,
+        inputs=[],
+        outputs=[name_input, affiliation_input]
+    )
+    gr.Examples(
+        examples=[["Raz Nissim", "Ben Gurion University, General Motors"]],
+        inputs=[name_input, affiliation_input]
+    )
+
 view.launch(inbrowser=True)
